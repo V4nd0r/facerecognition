fba06662f849d11ddde090ff50b6b5a1ae67f893
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2022-09-27 19:45:19 +0100

    Update App.js
    added face recognition

--- src/App.js
+++ src/App.js
@@ -2,22 +2,60 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/Facerecognition';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
 import './App.css';
 
-function App() {
+const app = new Clarifai.App({
+  apiKey:'a5631b3fd6d648ee9b29d320e93cdbb0'
+});
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input: '',
+      imageUrl: '',
+    }
+  }
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () =>{
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input
+      ).then (
+      function(response){
+        
+      },
+      function(err){
+
+      }
+    );
+    
+  }
+
+  render() {
   return (
     <div className="App">
       <ParticlesBg type='cobweb' bg={true}/>
       <Navigation />
       <Logo />
       <Rank/>
-      <ImageLinkForm />
-      {/*<FaceRecognition/>*/}
+      <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}/>
+      <FaceRecognition imageUrl={this.state.imageUrl}/>
 
     </div>
   );
 }
-
+}
 export default App;
