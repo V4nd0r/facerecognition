522b18738e70db7fafb39b9fa7a84aa6ea082442
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2022-10-16 19:25:07 +0100

    Update SignIn.js
    fixed a typo

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -12,13 +12,25 @@ class SignIn extends React.Component {
         this.setState({signInEmail:event.target.value})
     }
 
-    onPaswwordChange = (event) => {
+    onPasswordChange = (event) => {
         this.setState({signInPassword:event.target.value})
     }
 
     onSubmitSignIn = () => {
-        console.log(this.state);
-        this.props.onRouteChange('home');
+        fetch('http://localhost:3000/signin', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword,
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data === 'success') {
+                this.props.onRouteChange('home');
+            }
+        })
     }
 
     render() {
@@ -46,7 +58,7 @@ class SignIn extends React.Component {
                         type="password" 
                         name="password"  
                         id="password"
-                        onChange={this.onPaswwordChange}
+                        onChange={this.onPasswordChange}
                         />
                 </div>
                 </fieldset>
