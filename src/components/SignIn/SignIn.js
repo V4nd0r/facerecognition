0db831769836e5d5ca14d0bac2fb2021f32113c9
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2022-10-18 21:57:58 +0100

    Update SignIn.js

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor(loadUser) {
+        super(loadUser);
         this.state = {
             signInEmail: '',
             signInPassword: '',
