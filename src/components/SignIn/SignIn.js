7a76d2e1031cfd7217e91677be0fa4f8f6bbff02
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2022-10-19 18:42:25 +0100

    Update SignIn.js
    minor changes

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -26,8 +26,8 @@ class SignIn extends React.Component {
             })
         })
         .then(response => response.json())
-        .then(data => {
-            if (data === 'success') {
+        .then(user => {
+            if (user.id) {
                 this.props.onRouteChange('home');
             }
         })
