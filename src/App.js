3859c7fc2cbfefff646d48449f0bc98ab1fe1f6f
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2022-10-31 16:40:27 +0000

    deploy

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ loadUser = (data) => {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://pure-mesa-07357.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -73,7 +73,7 @@ loadUser = (data) => {
     .then(response => response.json())
     .then (response => {
         if(response){
-          fetch('http://localhost:3000/image', {
+          fetch('https://pure-mesa-07357.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
