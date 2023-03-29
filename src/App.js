4a6710d4b26599b1b714b16eee45c982a4df0dcb
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2023-03-29 08:33:25 +0100

    Revert "Updated Authorization"
    This reverts commit 575a67b784e80251636455bcaf98ea379bf9c9d9.

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const returnClarifaiRequestOptions = (imageUrl) =>  {
     method: 'POST',
     headers: {
         'Accept': 'application/json',
-        'Authorization': 'Key ' + PAT
+        'Authorization': PAT
     },
     body: raw
   };
